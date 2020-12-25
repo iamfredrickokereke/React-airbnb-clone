0be048d224e564d326db8ba86e9ecaace05f6eee
Kelly online <kellyonline2016@gmail.com>
2020-12-25 18:04:15 +0100

    created a Flat component and style sheet

---
+++ airbnb/src/components/Flat.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Flat extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Flat component!</h1>
+      </div>
+    );
+  }
+}
+
+export default Flat;
